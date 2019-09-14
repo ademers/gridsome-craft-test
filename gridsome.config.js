0faf9d4ad560fbbda9d0d952503ba76cefec344e
Andrea DeMers <andreademers@gmail.com>
2019-09-14 11:33:23 -0400

    Add Gridsome Tailwind CSS & Critical CSS plugins

--- gridsome.config.js
+++ gridsome.config.js
@@ -18,8 +18,28 @@ module.exports = {
         },
       },
     },
+
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: 'tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true,
+      }
+    },
+    
+    {
+      use: '@gridsome/plugin-critical',
+      options: {
+        paths: ['/'],
+        width: 1300,
+        height: 900
+      }
+    },    
   ],
 }
-
 
 
